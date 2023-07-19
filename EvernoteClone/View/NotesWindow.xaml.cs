b602b6caad0be013254579e18df6071cd811d308
Grzegorz <hvitr.ulfr@gmail.com>
2023-07-19 15:09:55 +0200

    azure speech to text added

--- EvernoteClone/View/NotesWindow.xaml.cs
+++ EvernoteClone/View/NotesWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows;
+﻿using Microsoft.CognitiveServices.Speech;
+using Microsoft.CognitiveServices.Speech.Audio;
+using System.Windows;
 using System.Windows.Documents;
 
 namespace EvernoteClone.View
@@ -18,11 +20,6 @@ namespace EvernoteClone.View
             Application.Current.Shutdown();
         }
 
-        private void speechButton_Click(object sender, RoutedEventArgs e)
-        {
-
-        }
-
         private void boldButton_Click(object sender, RoutedEventArgs e)
         {
             contentRichTextBox.Selection.ApplyPropertyValue(Inline.FontWeightProperty, FontWeights.Bold);
@@ -33,6 +30,22 @@ namespace EvernoteClone.View
             int ammountCharacters = (new TextRange(contentRichTextBox.Document.ContentStart, contentRichTextBox.Document.ContentEnd)).Text.Length;
 
             statusTextBlock.Text = $"Document length: {ammountCharacters} characters";
+        }
+
+        private async void azureSpeechButton_Click(object sender, RoutedEventArgs e)
+        {
+            string region = "northeurope";
+            string key = "f3116dbc3c63471b99ad27bde30480b7";
+
+            var speechConfig = SpeechConfig.FromSubscription(key, region);
+            using (var audioConfig = AudioConfig.FromDefaultMicrophoneInput())
+            {
+                using (var recognizer = new SpeechRecognizer(speechConfig, audioConfig))
+                {
+                    var result = await recognizer.RecognizeOnceAsync();
+                    contentRichTextBox.Document.Blocks.Add(new Paragraph(new Run(result.Text)));
+                }
+            }
         }
     }
 }
