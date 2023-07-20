d3ada433fab0b893db8db14656be5da96b9fbbda
Grzegorz <hvitr.ulfr@gmail.com>
2023-07-20 11:12:22 +0200

    WPF Behaviors NuGet package for triggering commads by events added; updating the database after renaming the notebook

---
+++ EvernoteClone/ViewModel/Commands/EndEditingCommand.cs
@@ -0,0 +1,29 @@
+﻿using EvernoteClone.Model;
+using System;
+using System.Windows.Input;
+
+namespace EvernoteClone.ViewModel.Commands
+{
+    public class EndEditingCommand : ICommand
+    {
+        public event EventHandler CanExecuteChanged;
+        public NotesVM VM { get; set; }
+
+        public EndEditingCommand(NotesVM vM)
+        {
+            VM = vM;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            var notebook = parameter as Notebook;
+            if (notebook != null)
+                VM.StopEditing(notebook);
+        }
+    }
+}
